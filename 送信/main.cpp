68a3ad2a78031751d08858ae9fae1a77ea075cfc
Arithmos-tools <chapuis@docomo.ne.jp>
2024-10-31 03:49:33 +0900

    掃除

--- 送信/main.cpp
+++ 送信/main.cpp
@@ -1,12 +1,14 @@
 #define IPC_CREAT 0x200
 #define IPC_EXCL 0x200
-struct msgbuf {
-    long mtype = 1;      
-    char message[22]; 
+struct msgbuf
+{
+    long mtype = 1;
+    char message[22];
 };
-struct __kernel_timespec {
+struct __kernel_timespec
+{
     unsigned long long tv_sec;
-    unsigned long long tv_nsec; 
+    unsigned long long tv_nsec;
 };
 constexpr unsigned long long strlen(const char *str)
 {
@@ -42,27 +44,27 @@ unsigned long long send(unsigned long long IPCid, msgbuf *buffer)
 {
     signed long long returnValue;
     asm volatile("movq %1, %%rdi\n\t"
-                 "movq $0x45, %%rax\n\t" 
+                 "movq $0x45, %%rax\n\t"
                  "movq %2, %%rdx\n\t"
                  "movq %3, %%rsi\n\t"
                  "movq $0, %%r10\n\t"
                  "syscall\n\t"
                  "movq %%rax, %0\n\t"
                  : "=r"(returnValue)
-                 : "r"(IPCid), "r"(sizeof(msgbuf)-8), "r"(buffer)
+                 : "r"(IPCid), "r"(sizeof(msgbuf) - 8), "r"(buffer)
                  : "%rdi", "%rax", "%rsi");
     return returnValue;
 }
 void sleep()
 {
-        __kernel_timespec time;
-        time.tv_sec = 1;
-        time.tv_nsec = 0;
-        asm volatile("movq %0, %%rdi\n\t"
-                 "movq $0x23, %%rax\n\t" 
+    __kernel_timespec time;
+    time.tv_sec = 1;
+    time.tv_nsec = 0;
+    asm volatile("movq %0, %%rdi\n\t"
+                 "movq $0x23, %%rax\n\t"
                  "movq $0, %%rsi\n\t"
                  "syscall\n\t"
-                 : 
+                 :
                  : "r"(&time)
                  : "%rdi", "%rax", "%rsi");
 }
@@ -82,17 +84,17 @@ int main()
         print("IPC queue already exists\n");
         exit(IPCid);
     }
-    while(true)
+    while (true)
     {
         msgbuf buffer;
-        const char* message = "Did you receive that ?";
-        for(int i = 0; i < strlen(message); ++i)
+        const char *message = "Did you receive that ?";
+        for (int i = 0; i < strlen(message); ++i)
             buffer.message[i] = message[i];
         signed long long returnValue = send(IPCid, &buffer);
-        if(returnValue >= 0)
+        if (returnValue >= 0)
             print("Message sent successfully\n");
         sleep();
     }
-    //相手のプロセスが情報を読み取るために信号を送ったほうがいいですが、この状況では、ターゲットプロセスのIDがわからないので、タイムアウトを使用します。
+    // 相手のプロセスが情報を読み取るために信号を送ったほうがいいですが、この状況では、ターゲットプロセスのIDがわからないので、タイムアウトを使用します。
     return 0;
 }
